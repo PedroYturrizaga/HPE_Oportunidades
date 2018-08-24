912acd31a926659edc8243b86a69f701a9429c44
Pedro Yturrizaga <blue2@MAC-BLUE2.local>
2018-08-24 12:28:28 -0500

    cambios

--- application/views/en/V_Champion.php
+++ application/views/en/V_Champion.php
@@ -57,10 +57,10 @@
                 <?php } ?>
             </div>
             <div class="formulario col-sm-12 col-xs-12 m-t-20">
-				<div class="col-sm-4 col-xs-12"> 
+                <div class="col-sm-4 col-xs-12"> 
                     <div id="venta" style="width: 350px; height: 250px;"></div>
                     <div id="puntaje" style="width: 350px; height: 250px;"></div>
-				</div>
+                </div>
                 <div class="col-sm-8 col-xs-12">
                     <div class="js-table"> 
                         <h2>Top 3 canales en importes facturados</h2>
@@ -81,7 +81,7 @@
                         </div>
                     </div>
                     <div class="js-table">
-                    	<h2>&Uacute;ltimos 10 registros </h2>
+                        <h2>&Uacute;ltimos 10 registros </h2>
                         <div class="table-responsive">
                             <table class="table" id="tableCotizacion">
                                 <thead>
@@ -139,100 +139,79 @@
             <div class="modal-content">
                 <div class="mdl-card">
                     <div class="mdl-card__title">
-                        <h2 class="title-formulario">Detalles de Oportunidad</h2>
+                        <h2 class="js-title">¡Gana <span id="money"></span> d&oacute;lares en puntos Engage & Grow por &oacute;rdenes superiores a los 15k en la promoci&oacute;n de <span id="promocion"></span>!</h2>
                     </div>
                     <div class="mdl-card__supporting-text">
-                        <div class="col-sm-6 col-xs-12"> 
+                        <div class="col-sm-6 col-xs-12">
                             <div class="col-xs-12 js-input">
-                                <label for="canal">Canal al que cotiza</label>
-                                <input class="js-disabled" type="text" id="canal">
+                                <label for="Nombre">Nombre</label>
+                                <input type="text" id="Nombre" onchange="validarCampos()">
                             </div>
                             <div class="col-xs-12 js-input">
-                                <label for="Nombre">Nombre del vendedor del canal</label>
-                                <input class="js-disabled" type="text" id="Nombre">
-                            </div>
-                            <div class="col-xs-12 js-input">
-                                <label for="email">Email (debe ser el mismo que tiene registrado en Engage & Grow)</label>
-                                <input class="js-disabled" type="email" id="email">
-                            </div>
-                            <div class="col-xs-12 js-input js-select">
-                                <select name="noMayorista" id="noMayorista" class="selectpicker js-disabled"></select>
+                                <label for="compania">Compa&ntilde;&iacute;a</label>
+                                <input type="text" id="compania" onchange="validarCampos()">
                             </div>
                             <div class="col-xs-12 js-input">
                                 <label for="pais">Pais</label>
-                                <input class="js-disabled" type="text"  id="pais">
+                                <input type="text" id="pais" onchange="validarCampos()">
                             </div>
-                            <div class="col-xs-12 js-input js-radio" onchange="calcularWSEE(); calcularWSSE(); calcularWSDE(); calcularCAL();">
-                                <label> Tipo de documento:</label>
-                                <label class="mdl-radio mdl-js-radio mdl-js-ripple-effect" for="radioCotizacion" id="cotizacion">
-                                    <input type="radio" id="radioCotizacion" class="mdl-radio__button" name="option1" value="1">
-                                    <span class="mdl-radio__label">Cotizaci&oacute;n</span>
+                            <div class="col-xs-12 js-input">
+                                <label for="email">Email</label>
+                                <input type="email" id="email" onchange="validarCampos()">
+                            </div>
+                            <div class="col-xs-12 js-input">
+                                <label for="telefono">Tel&eacute;fono</label>
+                                <input type="text" id="telefono" onchange="validarCampos()">
+                            </div>
+                            <div class="col-xs-12 js-input js-radio">
+                                <label>¿Tienes una cuenta de Engage&Grow activa?</label>
+                                <label class="mdl-radio mdl-js-radio mdl-js-ripple-effect" for="radioCotizacion">
+                                    <input type="radio" id="radioCotizacion" class="mdl-radio__button" name="option1" value="1" onchange="validarCampos()">
+                                    <span class="mdl-radio__label">Sí</span>
                                 </label>
-                                <label class="mdl-radio mdl-js-radio mdl-js-ripple-effect" for="radioFacturacion" id="facturacion">
-                                    <input type="radio" id="radioFacturacion" class="mdl-radio__button" name="option1" value="0">
-                                    <span class="mdl-radio__label">Factura</span>
+                                <label class="mdl-radio mdl-js-radio mdl-js-ripple-effect" for="radioFacturacion">
+                                    <input type="radio" id="radioFacturacion" class="mdl-radio__button" name="option1" value="0" onchange="validarCampos()">
+                                    <span class="mdl-radio__label">No</span>
                                 </label>
                             </div>
                         </div>
                         <div class="col-sm-6 col-xs-12">
+                            <div class="col-xs-12 js-input js-select">
+                                <select name="noMayorista" id="noMayorista"> 
+                                    <?php echo $option ?>
+                                </select>
+                            </div>
                             <div class="col-xs-12 js-input">
-                                <label for="numFactura"># de Documento</label>
-                                <input class="js-disabled" type="text"id="numFactura" >
+                                <label for="NombreContacto">Nombre de la persona que te atendi&oacute; dentro del mayorista</label>
+                                <input type="text" id="NombrePersona" onchange="validarCampos()">
+                            </div>
+                            <div class="col-xs-12 js-input">
+                                <label for="emailContacto">Email del contacto que te atendi&oacute; dentro del mayorista</label>
+                                <input type="email" id="emailContacto" onchange="validarCampos()">
+                            </div>
+
+                            <div class="col-xs-12 js-input">
+                                <label for="numFactura"># de la factura del mayorista con el que se factur&oacute;</label>
+                                <input type="text" id="numFactura" onchange="validarCampos()">
                             </div>
                             <div class="col-xs-12 js-input js-date js-flex">
-                                <input class="js-disabled" type="text" id="fecha" name="fecha" maxlength="10" placeholder="Fecha del documento" value="" style="pointer-events: none">
+                                <input type="text" id="fecha" name="fecha" maxlength="10" placeholder="Fecha de Facturación" value="" style="pointer-events: none">
                                 <div class="js-icon">
-                                    <button type="button" class="mdl-button mdl-js-button mdl-button--icon js-disabled">
+                                    <button type="button" class="mdl-button mdl-js-button mdl-button--icon">
                                         <i class="mdi mdi-date_range"></i>
                                     </button>
                                 </div>
                             </div>
                             <div class="col-xs-12 js-input">
-                                <label for="monto">Monto en USD</label>
-                                <input class="js-disabled" placeholder="0.00" type="text" id="monto">
+                                <label for="monto">Monto total de la orden de compra en USD</label>
+                                <input type="text" id="monto" placeholder="0.00" onchange="validarCampos()">
                             </div>
                             <div class="col-xs-12 js-input js-file js-flex">
-                                <input class="js-disabled" type="text" id="archivoDocumento" name="archivoDocumento" maxlength="10" placeholder="Suba su factura en imagen o pdf (2MB Max)" style="pointer-events: none">
+                                <input type="text" id="archivoDocumento" placeholder="Suba su factura en imagen o pdf (2MB Max)" name="archivoDocumento" disabled>
                                 <div class="js-icon">
-                                    <button type="button" class="mdl-button mdl-js-button mdl-button--icon js-disabled" onclick="subirFactura()">
+                                    <button type="button" class="mdl-button mdl-js-button mdl-button--icon" onclick="subirFactura()">
                                         <i class="mdi mdi-backup"></i>
                                     </button>
-                                </div>
-                            </div>
-                            <div class="js-table col-xs-12">
-                                <h2 class="title-formulario">Productos</h2>
-                                <div class="table-responsive">
-                                    <table id="tbProductos" class="table">
-                                        <thead>
-                                            <tr>
-                                                <th>Licencia Microsoft</th>
-                                                <th class="col-xs-2 text-right">Unidades</th>
-                                                <th class="col-xs-2 text-right js-none">Puntos</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr>
-                                                <td id="product1">Windows Server Essentials Edition</td>
-                                                <td class="text-right col-xs-2 js-table--input"><input class="js-disabled" type="text" id="cantidadWSEE" name="cantidadWSEE" autocomplete="off" onchange="calcularWSEE()"/></td>
-                                                <td class="text-right col-xs-2 js-table--input js-none"><span id="puntosWSEE"></span></td>
-                                            </tr>
-                                            <tr>
-                                                <td id="product2">Windows Server Standard Edition</td>
-                                                <td class="text-right col-xs-2 js-table--input"><input class="js-disabled" type="text" id="cantidadWSSE" name="cantidadWSSE" autocomplete="off" onchange="calcularWSSE()"/></td>
-                                                <td class="text-right col-xs-2 js-table--input js-none"><span id="puntosWSSE"></span></td>
-                                            </tr>
-                                            <tr>
-                                                <td id="product3">Windows Server Datacenter Edition</td>
-                                                <td class="text-right col-xs-2 js-table--input"><input class="js-disabled" type="text" id="cantidadWSDE" name="cantidadWSDE" autocomplete="off" onchange="calcularWSDE()"/></td>
-                                                <td class="text-right col-xs-2 js-table--input js-none"><span id="puntosWSDE"></span></td>
-                                            </tr>
-                                            <tr>
-                                                <td id="product4">CALs</td>
-                                                <td class="text-right col-xs-2 js-table--input"><input class="js-disabled type="text" id="cantidadCAL" name="cantidadCAL" autocomplete="off" onchange="calcularCAL()"/></td>
-                                                <td class="text-right col-xs-2 js-table--input js-none"><span id="puntosCAL"></span></td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
                                 </div>
                             </div>
                         </div>
@@ -242,8 +221,7 @@
                     </div>
                     <div class="mdl-card__actions" id="registrar">                         
                         <button type="button" name="boton" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect js-button js-button--default" data-dismiss="modal">Cancelar</button>
-                        <button type="button" name="boton" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect js-button"  onclick="registrar(); agregarDatos(); ">Registrar Oportunidad</button>  
-                        <!-- onclick="agregarDatos(); ">Registrar Oportunidad</button> -->
+                        <button type="button" name="boton" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect js-button"  onclick="registrar(); agregarDatos(); ">Registrar Oportunidad</button>
                     </div>
                 </div>
             </div>
@@ -270,12 +248,12 @@
         <input id="archivo" type="file" name="archivo" />
         <input type="hidden" name="MAX_FILE_SIZE" value="2000000"/>
     </form>
-	<script src="<?php echo RUTA_JS?>jquery-3.2.1.min.js?v=<?php echo time();?>"></script>
-	<script src="<?php echo RUTA_JS?>jquery-1.11.2.min.js?v=<?php echo time();?>"></script>
-	<script src="<?php echo RUTA_PLUGINS?>bootstrap/js/bootstrap.min.js?v=<?php echo time();?>"></script>
-	<script src="<?php echo RUTA_PLUGINS?>bootstrap-select/js/bootstrap-select.min.js?v=<?php echo time();?>"></script>
-	<script src="<?php echo RUTA_PLUGINS?>bootstrap-select/js/i18n/defaults-es_ES.min.js?v=<?php echo time();?>"></script>
-	<script src="<?php echo RUTA_PLUGINS?>mdl/material.min.js?v=<?php echo time();?>"></script>
+    <script src="<?php echo RUTA_JS?>jquery-3.2.1.min.js?v=<?php echo time();?>"></script>
+    <script src="<?php echo RUTA_JS?>jquery-1.11.2.min.js?v=<?php echo time();?>"></script>
+    <script src="<?php echo RUTA_PLUGINS?>bootstrap/js/bootstrap.min.js?v=<?php echo time();?>"></script>
+    <script src="<?php echo RUTA_PLUGINS?>bootstrap-select/js/bootstrap-select.min.js?v=<?php echo time();?>"></script>
+    <script src="<?php echo RUTA_PLUGINS?>bootstrap-select/js/i18n/defaults-es_ES.min.js?v=<?php echo time();?>"></script>
+    <script src="<?php echo RUTA_PLUGINS?>mdl/material.min.js?v=<?php echo time();?>"></script>
     <script src="<?php echo RUTA_PLUGINS?>moment/moment.min.js?v=<?php echo time();?>"></script>
     <script src="<?php echo RUTA_PLUGINS?>datetimepicker/js/bootstrap-material-datetimepicker.js?v=<?php echo time();?>"></script>
     <script src="<?php echo RUTA_PLUGINS?>google_chart/loader.js?v=<?php echo time();?>"></script>
@@ -297,8 +275,8 @@
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
 
     <script type="text/javascript">
-    	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
-        	$('select').selectpicker('mobile');
+        if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
+            $('select').selectpicker('mobile');
         } else {
             $('select').selectpicker();
         }
