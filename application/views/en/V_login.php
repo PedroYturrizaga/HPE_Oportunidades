bebcb74a0febf402fed88da2c2778288ad02f9a6
joseminaya15 <jose.minayac15@gmail.com>
2018-08-28 12:59:13 -0500

    terminado

--- application/views/en/V_login.php
+++ application/views/en/V_login.php
@@ -130,6 +130,10 @@
         <script src="<?php echo RUTA_JS?>Utils.js?v=<?php echo time();?>"></script>
         <script src="<?php echo RUTA_JS?>login.js?v=<?php echo time();?>"></script>
         <script type="text/javascript">
+            var URLactual = window.location;
+            if(URLactual['href'] != 'http://www.marketinghpe.com/promostorageq4/rla/en/'){
+                location.href = 'http://www.marketinghpe.com/promostorageq4/rla/en/';
+            }
             if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
                 $('select').selectpicker('mobile');
             } else {
